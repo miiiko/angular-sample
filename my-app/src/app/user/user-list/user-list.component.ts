9100284238908ee8da03c5e7eb28e6c3ef9f0bb6
miko <miiiko_service@icloud.com>
2019-06-23 15:52:06 +0900

    リストを表示する

--- my-app/src/app/user/user-list/user-list.component.ts
+++ my-app/src/app/user/user-list/user-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export class User {
+  id: number;
+  name: string;
+  email: string;
+}
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
@@ -8,8 +13,15 @@ import { Component, OnInit } from '@angular/core';
 export class UserListComponent implements OnInit {
 
   constructor() { }
+  users: User[];
 
   ngOnInit() {
+    this.users = [
+      { id: 1, name: 'Yamada', email: 'yamada@example.com' },
+      { id: 2, name: 'Suzuki', email: 'suzuki@example.com' },
+      { id: 3, name: 'Tanaka', email: 'tanaka@example.com' },
+    ];
   }
 
 }
+
